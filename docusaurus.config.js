f0fcc4b320ae255a6e75bdcd4eb2f3fe431de3ba
tpascarella <pascarella96@gmail.com>
2021-07-26 16:52:10 -0500

    changed logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/sr50logo.ico',
   organizationName: 'tpascarella', // Usually your GitHub org/user name.
   projectName: 'sr50', // Usually your repo name.
   themeConfig: {
