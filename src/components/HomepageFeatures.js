3145bc7c904f09060c9cfe0c9cca668a593a16ad
tpascarella <pascarella96@gmail.com>
2021-07-26 16:24:18 -0500

    updated styling, added logo, updated homepage icons

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,16 +5,18 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Open Source',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('../../static/img/public_white_24dp.svg').default, 
+    /* Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default, */
     description: (
       <>
-        SR50 is open source, meaning we get contributions on how to improve the method from people all over the world. We are constantly refining the method to deliver the most efficient personal financial method in existence.
+        SR50 is open source, meaning we get contributions on how to improve the method from people all over the world. We are constantly refining the method to deliver the most efficient personal financial method in existence, and it's only getting better.
       </>
     ),
   },
   {
     title: 'Transparent',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('../../static/img/search_white_24dp.svg').default,
+    /* Svg: require('../../static/img/undraw_docusaurus_tree.svg').default, */
     description: (
       <>
         Every financial advisor in the country is an insurance salesman posing as a financial advisor. Look up the job postings if you don't believe me. We are completely transparent about how we make money, what we teach, and why.
@@ -23,7 +25,8 @@ const FeatureList = [
   },
   {
     title: 'Secure',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('../../static/img/lock_white_24dp.svg').default,
+    /* Svg: require('../../static/img/undraw_docusaurus_react.svg').default, */
     description: (
       <>
         Modern finance apps force you to give them your personal login information in order to provide value. We collect zero data from users.
