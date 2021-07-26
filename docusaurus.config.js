22eaea7769f0d9cca6f6daa33f87b474702ab0be
tpascarella <pascarella96@gmail.com>
2021-07-26 16:26:32 -0500

    added logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ module.exports = {
       title: 'SR50',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/sr50logo.svg',
       },
       items: [
         {
