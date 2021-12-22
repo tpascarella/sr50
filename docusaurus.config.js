f9e818333fd1cc6f45cd6b0ae1af2b7db97180f2
Thomas <pascarella96@gmail.com>
2021-12-21 22:16:11 -0600

    fix broken edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/tpascarella/sr50',
           // editUrl:
           //  'https://github.com/facebook/docusaurus/edit/master/website/', 
         },
@@ -99,7 +99,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/tpascarella/sr50',
           //editUrl:
           //  'https://github.com/facebook/docusaurus/edit/master/website/blog/', 
         },
