1bf1c91a0f89a1858fe44ec727137f26ed9df40a
Thomas Pascarella <pascarella96@gmail.com>
2022-06-26 14:01:09 -0500

    content update

---
+++ src/components/TechnicalNote.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TechnicalNote() {
+  return (
+    <div> 
+      <p id="technical-note">
+        ***This website is optimized for the Brave Browser. If you experience bugs using a different browser, switch browsers.***
+      </p>
+    </div>
+  );
+}
