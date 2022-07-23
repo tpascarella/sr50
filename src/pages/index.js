6337a3ed5b3d911b04e53bad834fa0f60053af32
Thomas Pascarella <pascarella96@gmail.com>
2022-07-23 12:38:23 -0500

    design changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures/HomepageFeatures';
-import HomepageHero from "../components/HomepageHero/HomepageHero";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
